2127a8988c08470db5437c2013c9dc06744a0f38
LeoZuzi <leonardozuzi0@gmail.com>
2024-12-11 23:29:03 -0300

    "parte 2 modulo 1"

---
+++ teste-processamento/src/Main.java
@@ -0,0 +1,28 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+    int x, y;
+    x = 5;
+    y = 2 * x;
+
+    System.out.println(x);
+    System.out.println(y);
+
+    double b, B, h, area;
+
+    b = 6.0;
+    B = 8.0;
+    h = 5.0;
+    area = (b + B)/2.0 * h;
+    System.out.println(area);
+
+    int c,d ;
+    double resultado;
+    c = 5;
+    d =2;
+    resultado = (double) c/d;
+    System.out.println(resultado);
+    }
+}
